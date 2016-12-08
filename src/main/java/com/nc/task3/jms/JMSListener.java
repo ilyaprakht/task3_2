a5c3372d3cf77f48247dc8e87ebbaf89a546b530
ilyaprakht <ilyaprakht@gmail.com>
2016-12-08 17:33:39 +0400

    added transaction

--- src/main/java/com/nc/task3/jms/JMSListener.java
+++ src/main/java/com/nc/task3/jms/JMSListener.java
@@ -1,10 +1,12 @@
 package com.nc.task3.jms;
 
 import com.nc.task3.entities.CityWeather;
+import org.springframework.transaction.annotation.Transactional;
 
 
 public interface JMSListener {
 
+    @Transactional
     void handleMessage(CityWeather cityWeather);
 
 }
